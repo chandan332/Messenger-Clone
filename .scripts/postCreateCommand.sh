cc383b08e74b5928db07fc2ebc4043a73ab2a523
Chandan Karmakar <roysiddhart332@gmail.com>
2024-11-02 08:20:09 +0000

    First commit

---
+++ .scripts/postCreateCommand.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+composer install
+
+npm install
+
+chmod +x .scripts/*
+
+if ! ln -s .env.example .env; then
+    rm -rf .env
+    ln -s .env.example .env
+fi
+
+php artisan migrate --force
